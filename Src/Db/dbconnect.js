1117e3fc842e728e475296de8e7fa480b8c2010a
ankitasGIT <ankitalonkar101@gmail.com>
2024-07-19 17:23:07 +0530

    first commit

---
+++ Src/Db/dbconnect.js
@@ -0,0 +1,15 @@
+
+import mongoose from "mongoose";
+import { dbname } from "../constants.js";
+
+const connectDb = async() => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI_NAME}/${dbname}`);
+
+        console.log(`MongoDB connected !! MONGODB HOST : ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log('MongoDB Connection error : '  ,error);
+    }
+}
+
+export default connectDb;
---
+++ Src/index.js
@@ -0,0 +1,19 @@
+import {app} from "./app.js";
+import dotenv from "dotenv";
+import connectDb from "./Db/dbconnect.js";
+
+dotenv.config({
+    path: '/.env'
+})
+
+
+connectDb()
+.then( () => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running on ${process.env.PORT}`);
+        })
+    })
+.catch( (error) => {
+    console.log("MONGODB connection went wrong!!", error)
+} )
+
